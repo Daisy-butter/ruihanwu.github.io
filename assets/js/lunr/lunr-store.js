f0fbcb0ee249e3d994987c1f8677770e6990a224
Daisy-butter <Daisy-butter@users.noreply.github.com>
2025-10-26 06:46:23 +0000

    deploy: de29ed59b324249a18ca06df6336c064821a140e

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "RoboBrain 2.0: Embodied Intelligence with Multimodal Models",
+        "excerpt":"RoboBrain 2.0 integrates visual-language-action reasoning through multimodal world models.  It enables agents to learn interactive tasks grounded in perception and embodiment…  ","categories": [],
+        "tags": [],
+        "url": "/2025/10/26/robobrain.html",
+        "teaser": null
+      }]
